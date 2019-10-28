c28b3b5ec3c2579a651c74574c7ff07d34ba5f9d
FPT Aptech Nguyễn Thành Đạt <datntd00498@fpt.edu.vn>
2019-10-28 11:02:41 +0700

    login-register

--- src/main/java/t1708m/fashion/controller/ClientController.java
+++ src/main/java/t1708m/fashion/controller/ClientController.java
@@ -1,6 +1,8 @@
 package t1708m.fashion.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,4 +14,10 @@ public class ClientController {
     public String getClient() {
         return "client/index";
     }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
 }
--- src/main/java/t1708m/fashion/entity/Account.java
+++ src/main/java/t1708m/fashion/entity/Account.java
@@ -7,10 +7,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 @Data
 @Getter
 @Setter
