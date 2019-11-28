a8e63b45b2907ffd66b59a3efe5a79391eb9ff22
FPT Aptech Nguyễn Thành Đạt <datntd00498@fpt.edu.vn>
2019-11-28 18:09:08 +0700

    update cart dang error

--- src/main/java/t1708m/fashion/controller/BlogController.java
+++ src/main/java/t1708m/fashion/controller/BlogController.java
@@ -2,12 +2,19 @@ package t1708m.fashion.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import t1708m.fashion.Specification.ProductSpecification;
+import t1708m.fashion.Specification.SearchCriteria;
 import t1708m.fashion.entity.Article;
+import t1708m.fashion.entity.Product;
 import t1708m.fashion.service.BlogService;
 import java.util.List;
 
@@ -17,10 +24,22 @@ public class BlogController {
     @Autowired
     BlogService blogService;
 
+
     @RequestMapping(method = RequestMethod.GET)
-    public String index(Model model) {
-        List<Article> blog = blogService.blog();
-        model.addAttribute("blogs", blog);
+    public String category(
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "limit", defaultValue = "6") int limit,
+            Model model, @RequestParam(defaultValue = "") String articleName) {
+        Specification specification = Specification.where(null);
+
+
+        Page<Article> articlePage = blogService.articles(specification,PageRequest.of(page - 1, limit));
+
+        model.addAttribute("blogs", articlePage);
+
+        model.addAttribute("currentPage", articlePage.getPageable().getPageNumber() + 1);
+        model.addAttribute("limit", articlePage.getPageable().getPageSize());
+        model.addAttribute("totalPage", articlePage.getTotalPages());
         return "/client/blog";
     }
 
--- src/main/java/t1708m/fashion/controller/CartController.java
+++ src/main/java/t1708m/fashion/controller/CartController.java
@@ -3,18 +3,18 @@ package t1708m.fashion.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import t1708m.fashion.entity.HelloOrder;
 import t1708m.fashion.entity.HelloOrderDetail;
 import t1708m.fashion.entity.Product;
 import t1708m.fashion.entity.ShopingCart;
 import t1708m.fashion.service.ProductService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -44,6 +44,37 @@ public class CartController {
         cart.addProduct(product, quantity);
         session.setAttribute(SHOPPING_CART_ATTRIBUTE, cart);
         return "Okie";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/update/{id}")
+    public String updateCart(HttpSession session,Model model){
+        ShopingCart cart = loadCart(session);
+        model.addAttribute("cart", cart);
+        session.setAttribute("cart", cart);
+        for (HelloOrderDetail orderDetail : cart.getItems()) {
+            System.out.println("ID: " + orderDetail.getId());
+        }
+        System.out.println("Price: " + cart.getTotalPrice());
+        return "/client/shoping-cart";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/update")
+    public String updateCart1(HttpSession session,
+                         HttpServletResponse response,
+                         @RequestParam(name = "productId") long productId,
+                         @RequestParam(name = "quantity", defaultValue = "1") int quantity){
+        if (quantity <0 ){
+            return "NOT FOUND";
+        }
+        Product product = productService.getById(productId);
+        if (product == null) {
+            response.setStatus(404);
+            return "NOT FOUND";
+        }
+        ShopingCart cart = loadCart(session);
+        cart.updateProduct(productId,quantity);
+        session.setAttribute(SHOPPING_CART_ATTRIBUTE, cart);
+        return "redirect:/client/shoping-cart";
     }
 
 
